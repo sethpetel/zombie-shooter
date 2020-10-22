ca3fedb31cc4672e62f0f1d2724d2c9ea8bb26e4
sethpetel <44174039+sethpetel@users.noreply.github.com>
2020-10-22 18:32:39 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
-
+et mySprite: Sprite = null
+let zombieCounter = 0
 let zombieImgs = [
 img`
     ...cccccccccccccc...
@@ -303,17 +304,37 @@ img`
     .....ccc.....e......
     `
 ]
+let mySprite2 = sprites.create(img`
+    . . . . . . f f f f . . . . . .
+    . . . . f f f 2 2 f f f . . . .
+    . . . f f f 2 2 2 2 f f f . . .
+    . . f f f e e e e e e f f f . .
+    . . f f e 2 2 2 2 2 2 e e f . .
+    . . f e 2 f f f f f f 2 e f . .
+    . . f f f f e e e e f f f f . .
+    . f f e f b f 4 4 f b f e f f .
+    . f e e 4 1 f d d f 1 4 e e f .
+    . . f e e d d d d d d e e f . .
+    . . . f e e 4 4 4 4 e e f . . .
+    . . e 4 f 2 2 2 2 2 2 f 4 e . .
+    . . 4 d f 2 2 2 2 2 2 f d 4 . .
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . .
+    . . . . . f f f f f f . . . . .
+    . . . . . f f . . f f . . . . .
+`)
+controller.moveSprite(mySprite2)
+scene.cameraFollowSprite(mySprite2)
 scene.setBackgroundColor(7)
 tiles.setTilemap(tilemap`level_1`)
 let waveNumber = 1
-let zombieCounter = 0
-
 function zombieSpawn(zombies: any[]){
     for(let i = 0; i < zombies.length; i++){
         let tempImg = zombies.get(i)
         for (let j = 0; j < waveNumber; j++){
-            
+           tiles.placeOnRandomTile( sprites.create(tempImg , SpriteKind.Enemy),  sprites.dungeon.floorMixed)
         }
     }
 }
-
+if (zombieCounter == 0 ){
+    zombieSpawn( zombieImgs )
+}
